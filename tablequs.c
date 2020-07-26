8643bd6c5140a67edfbae7dca208c50357ec1d67
Deepakpatel001 <64587700+Deepakpatel001@users.noreply.github.com>
2020-07-27 00:32:18 +0530

    Add files via upload

---
+++ tablequs.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int a,i,j;
+printf("Enter your Last no.");
+scanf("%d",&a);
+for(i=1;i<=10;i++)
+{
+    for(j=1;j<=a;j++){
+        printf("%d x %d = %d\t",j,i,i*j);
+    }
+    printf("\n");
+}
+}
