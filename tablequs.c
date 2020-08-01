c1b11d2fded22427064babbad25d0cb0a1c23957
Deepakpatel001 <64587700+Deepakpatel001@users.noreply.github.com>
2020-08-01 21:50:49 +0530

    Update tablequs.c

--- tablequs.c
+++ tablequs.c
@@ -3,14 +3,12 @@
 int main(){
     int a,i,j;
 printf("Enter your Last no.");
-    printf("\t");
 scanf("%d",&a);
 for(i=1;i<=10;i++)
 {
     for(j=1;j<=a;j++){
         printf("%d x %d = %d\t",j,i,i*j);
     }
-    printf("\n");
-    
+    printf("\n");  
 }
 }
